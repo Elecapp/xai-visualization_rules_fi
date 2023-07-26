89a32e61f549ccca0e379f89672101d17f00e9b8
Salvo Rinzivillo <rinzivillo@isti.cnr.it>
2023-07-26 17:18:28 +0200

    moving visualization of bar of feature importance in a dedicated module

--- d3_sandbox/src/main.js
+++ d3_sandbox/src/main.js
@@ -2,46 +2,67 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 const d3 = require('d3');
 
-function FIPERFeatureView() {
-  let width = 500;
-  let height = 500;
-  const firstColumnWidth = 100;
-  const barLength = d3.scaleLinear().range([0, firstColumnWidth / 2]);
+const SINGLE_FEATURE_HEIGHT = 45;
 
+const FIRST_COLUMN_WIDTH = 100;
+
+function FIPERFeatureImportanceView() {
+  let width = FIRST_COLUMN_WIDTH;
+  let height = 50;
+  let fiExtent = [0, 1];
+  const barLength = d3.scaleLinear()
+    .range([0, width / 2])
+    .domain(fiExtent);
+
+
+  /**
+   * This function receives one single ```g``` element and visualizes its
+   * content using the associated data.
+   * @param selection the element containing a single datum with the
+   *  metadata of the feature to be visualized.
+   */
   function me(selection) {
-    const feature = selection.data();
-    const fiExtent = d3.extent(feature, d => d.feature_importance);
-    barLength.domain(fiExtent);
     selection
       .append('line')
-      .attr('x1', firstColumnWidth / 2)
-      .attr('x2', firstColumnWidth / 2)
+      .attr('x1', width / 2)
+      .attr('x2', width / 2)
       .attr('y1', 0)
-      .attr('y2', 40)
+      .attr('y2', SINGLE_FEATURE_HEIGHT)
       .attr('stroke', 'black');
     selection.selectAll('rect')
       .data(d => [d])
       .join('rect')
-      .attr('x', d => (firstColumnWidth / 2))
+      .attr('x', (width / 2))
       .attr('width', d => barLength(Math.abs(d.feature_importance) * (d.feature_importance < 0 ? -1 : 1)))
       .attr('height', 20)
       .attr('fill', d => (d.feature_importance < 0 ? 'red' : 'blue'));
     selection.append('text')
-      .attr('x', firstColumnWidth + 10)
-      .attr('y', 45/2)
+      .attr('x', width + 10)
+      .attr('y', SINGLE_FEATURE_HEIGHT / 2)
       .attr('dy', '-0.35em')
       .text(d => d.rname);
   }
 
+  // eslint-disable-next-line
   me.width = function (_) {
     if (!arguments.length) return width;
     width = _;
+    barLength.range([0, width / 2]);
     return me;
   };
 
+  // eslint-disable-next-line
   me.height = function (_) {
     if (!arguments.length) return height;
     height = _;
+    return me;
+  };
+
+  // eslint-disable-next-line
+  me.fitExtent = function (_) {
+    if (!arguments.length) return fiExtent;
+    fiExtent = _;
+    barLength.domain(fiExtent);
     return me;
   };
 
@@ -55,7 +76,12 @@ function FIPERView() {
   function me(selection) {
     console.log(selection.datum());
     const features = selection.datum();
-    const ffv = FIPERFeatureView().width(width).height(45);
+    const fiExtent = d3.extent(features, d => d.feature_importance);
+    const ffv = FIPERFeatureImportanceView()
+      .width(FIRST_COLUMN_WIDTH)
+      .height(SINGLE_FEATURE_HEIGHT)
+      .fitExtent(fiExtent);
+
     yScale.domain([0, features.length])
       .range([0, height]);
     const gFeatures = selection.selectAll('g')
@@ -63,7 +89,9 @@ function FIPERView() {
       .join('g')
       .classed('feature', true)
       .attr('transform', (d, i) => `translate(0, ${yScale(i)})`);
-    gFeatures.call(ffv);
+    gFeatures.each((_, j, n) => {
+      d3.select(n[j]).call(ffv);
+    });
   }
 
   // eslint-disable-next-line
