edaa27385774a6671a7882f738a36589f9301910
Salvo Rinzivillo <rinzivillo@isti.cnr.it>
2023-07-26 19:34:47 +0200

    completing visualization for categorical values introducing numerical values

--- d3_sandbox/src/main.js
+++ d3_sandbox/src/main.js
@@ -6,6 +6,62 @@ const SINGLE_FEATURE_HEIGHT = 25;
 const FIRST_COLUMN_WIDTH = 100;
 const SECOND_COLUMN_WIDTH = 300;
 
+// Format the data (instead of using d3.stack()) and
+// filter out 0 values:
+// extracted from: https://observablehq.com/@eesur/d3-single-stacked-bar
+function prepareCategoricalValues(data) {
+  const total = d3.sum(data, d => d.eda.count);
+
+  // use a scale to get percentage values
+  const percent = d3.scaleLinear()
+    .domain([0, total])
+    .range([0, 100]);
+  // filter out data that has zero values
+  // also get mapping for next placement
+  // (save having to format data for d3 stack)
+  let cumulative = 0;
+  data.sort((a, b) => b.eda.count - a.eda.count);
+  return data.map((d) => {
+    cumulative += d.eda.count;
+    return {
+      value: d.eda.count,
+      // want the cumulative to prior value (start of rect)
+      cumulative: cumulative - d.eda.count,
+      label: d.eda.category,
+      percent: percent(d.eda.count),
+      instance_value: d.instance_value,
+    };
+  }).filter(d => d.value > 0);
+}
+
+function prepareNumericalValues(data) {
+  const eda = data[0].eda;
+  const valuePoints = ['min', 'q1', 'median', 'q3', 'max'];
+  const newdata = [];
+  newdata.push({
+    value0: eda.min,
+    value1: eda.q1,
+    type: 'line'
+  });
+  newdata.push({
+    value0: eda.q1,
+    value1: eda.median,
+    type: 'box',
+  });
+  newdata.push({
+    value0: eda.median,
+    value1: eda.q3,
+    type: 'box',
+  });
+  newdata.push({
+    value0: eda.q3,
+    value1: eda.max,
+    type: 'line',
+  });
+
+  return newdata;
+}
+
 function FIPERFeatureValuesView() {
   let width = SECOND_COLUMN_WIDTH;
   let height = 50;
@@ -14,14 +70,49 @@ function FIPERFeatureValuesView() {
     .domain([0, 1]);
 
   function me(selection) {
-    selection.selectAll('rect')
-      .data(d => [d])
-      .join('rect')
-      .attr('x', 0)
-      .attr('y', SINGLE_FEATURE_HEIGHT / 4)
-      .attr('width', barLength(1))
-      .attr('height', SINGLE_FEATURE_HEIGHT * 2 / 3)
-      .attr('fill', 'lightgray');
+    if (selection.datum().type === 'categorical') {
+      const total = d3.sum(selection.datum().values, d => d.eda.count);
+      barLength.domain([0, total]);
+      selection.selectAll('rect')
+        .data(d => prepareCategoricalValues(d.values))
+        .join('rect')
+        .attr('x', d => barLength(d.cumulative))
+        .attr('y', SINGLE_FEATURE_HEIGHT / 4)
+        .attr('width', d => barLength(d.value))
+        .attr('height', SINGLE_FEATURE_HEIGHT * 2 / 3)
+        .attr('fill', 'lightgray')
+        .attr('stroke', 'white');
+
+      // draw the symbol for the actual value of the instance
+      selection.selectAll('circle')
+        .data(d => prepareCategoricalValues(d.values).filter(d => d.instance_value > 0))
+        .join('circle')
+        .attr('cx', d => barLength(d.cumulative) + barLength(d.value) / 2)
+        .attr('cy', SINGLE_FEATURE_HEIGHT / 2)
+        .attr('r', SINGLE_FEATURE_HEIGHT / 6)
+        .attr('fill', 'black');
+    } else {
+      barLength.domain([selection.datum().values[0].eda.min, selection.datum().values[0].eda.max]);
+      selection.selectAll('rect')
+        .data(d => prepareNumericalValues(d.values).filter(d => d.type === 'box'))
+        .join('rect')
+        .attr('x', d => barLength(d.value0))
+        .attr('y', SINGLE_FEATURE_HEIGHT / 4)
+        .attr('width', d => barLength(d.value1) - barLength(d.value0))
+        .attr('height', SINGLE_FEATURE_HEIGHT * 2 / 3)
+        .attr('fill', 'lightgray')
+        .attr('stroke', 'white');
+      selection.selectAll('line')
+        .data(d => prepareNumericalValues(d.values).filter(d => d.type === 'line'))
+        .join('line')
+        .attr('x1', d => barLength(d.value0))
+        .attr('x2', d => barLength(d.value1))
+        .attr('y1', SINGLE_FEATURE_HEIGHT / 2)
+        .attr('y2', SINGLE_FEATURE_HEIGHT / 2)
+        .attr('stroke', 'lightgray')
+        .attr('stroke-width', 1.3);
+    }
+
     return me;
   }
 
@@ -110,8 +201,10 @@ function FIPERFeatureImportanceView() {
   return me;
 }
 
+const GLOBAL_WIDTH = 700;
+
 function FIPERView() {
-  let width = 500;
+  let width = GLOBAL_WIDTH;
   let height = 500;
   const yScale = d3.scaleLinear();
   function me(selection) {
@@ -178,9 +271,9 @@ d3.json('/static/instance_34.json').then((data) => {
   const height = rEntries.length * SINGLE_FEATURE_HEIGHT;
   const svg = d3.select('#app')
     .append('svg')
-    .attr('width', 500)
+    .attr('width', GLOBAL_WIDTH)
     .attr('height', height);
 
-  const fv = FIPERView().width(500).height(height);
+  const fv = FIPERView().width(GLOBAL_WIDTH).height(height);
   svg.datum(rEntries).call(fv);
 });
