7c6d0f01be2b3fc56d72e4c50b580237cbe96c51
Eleonora Cappuccio <eleonora.cappuccio@phd.unipi.it>
2025-03-07 17:18:10 +0100

    trying to use the new LORE genetiv generator to export a Json for D3-Problem with SHAP

--- notebooks/JSONExporter_LORE_sa.py
+++ notebooks/JSONExporter_LORE_sa.py
@@ -2,26 +2,31 @@ import random
 
 import pandas as pd
 import numpy as np
-import xailib.xailib_base
+import shap
 
-from sklearn import preprocessing
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
-from xailib.data_loaders.dataframe_loader import prepare_dataframe
 from lore_sa.neighgen import GeneticGenerator
-from xailib.explainers.lime_explainer import LimeXAITabularExplainer
-from xailib.explainers.lore_explainer import LoreTabularExplainer
-from xailib.explainers.shap_explainer_tab import ShapXAITabularExplainer
-from lore_explainer.explanation import ExplanationEncoder
-from xailib.models.sklearn_classifier_wrapper import sklearn_classifier_wrapper
 
-import altair as alt
+
+from dataset import TabularDataset
+from neighgen import genetic
+from encoder_decoder import ColumnTransformerEnc
+from lore import Lore
+from surrogate import DecisionTreeSurrogate
+
+from lore_sa.bbox import sklearn_classifier_bbox
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler, OrdinalEncoder
+from sklearn.pipeline import make_pipeline
+from sklearn.compose import make_column_selector
+
+
 import json
-import matplotlib.pyplot as plt
+
 
 import os
 
@@ -31,6 +36,8 @@ path = os.getcwd()
 
 def load_data_from_csv(class_field, number_of_dataset):
     datasets = ['titanic_c.csv','german_credit.csv','abalone.csv','iris.csv']
+
+
     source_file = f'../datasets/{datasets[number_of_dataset]}'
     # Load and transform dataset
     df = pd.read_csv(source_file, skipinitialspace=True, na_values='?', keep_default_na=True)
@@ -38,34 +45,32 @@ def load_data_from_csv(class_field, number_of_dataset):
         df['Rings'] = pd.cut(df['Rings'],
                              bins=[-np.inf, 8, 10, np.inf],
                              labels=['young', 'medium', 'old'])
-    return df, class_field
-
-def train_model(df, class_field):
-    df, feature_names, class_values, numeric_columns, rdf, real_feature_names, features_map = prepare_dataframe(df, class_field)
-
-    test_size = 0.3
-    random_state = 42
-    X_train, X_test, Y_train, Y_test= train_test_split(df[feature_names], df[class_field],
-                                                        test_size=test_size,
-                                                        random_state=random_state,
-                                                        stratify=df[class_field])
-
-    bb = RandomForestClassifier(n_estimators=30, random_state=42)
-    bb.fit(X_train.values, Y_train.values)
-    bbox = sklearn_classifier_wrapper(bb)
-
-    s_explainer = ShapXAITabularExplainer(bbox, feature_names)
-    config = {'explainer': 'tree', 'X_train': X_train.iloc[0:].values}
-    s_explainer.fit(config)
-
-
-    l_explainer = LoreTabularExplainer(bbox)
-    config = {'neigh_type':'geneticp', 'size':100000, 'ocr':0.1, 'ngen':15}
-    l_explainer.fit(df, class_field, config)
+    df_prep = df.drop(columns=[class_field])
+    num_indices = [df_prep.columns.get_loc(col) for col in df_prep.select_dtypes(include=[np.number]).columns]
+    cat_indices = [df_prep.columns.get_loc(col) for col in df_prep.select_dtypes(exclude=[np.number]).columns]
 
 
 
-    return X_test, Y_test, l_explainer, s_explainer,bb, feature_names, numeric_columns,real_feature_names
+    preprocessor = ColumnTransformer(transformers=[
+        ('num', StandardScaler(),num_indices), #adapts to different dataset
+        ('cat',OrdinalEncoder(), cat_indices)
+    ])
+
+    return df, preprocessor, class_field
+
+
+
+def train_model(df, preprocessor, class_field):
+    model = make_pipeline(preprocessor, RandomForestClassifier(n_estimators=100, random_state=42))
+    X = df.drop(columns=[class_field]).values  # Select all features except target
+    y = df[class_field].values
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                        test_size=0.3, random_state=42, stratify=df[class_field].values)
+    model.fit(X_train, y_train)
+
+    return model, X_test, X_train
+
 
 class CustomJSONEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -82,23 +87,40 @@ class CustomJSONEncoder(json.JSONEncoder):
         return super(CustomJSONEncoder, self).default(obj)
 
 
-def select_and_explain_instance(inst_num, l_explnr, s_explnr, path, X_test, Y_test, bb, feature_names,real_feature_names, X_train,numeric_columns):
-    inst = X_test.iloc[inst_num].values
-    true_class = Y_test.iloc[inst_num]
+def select_and_explain_instance(number_of_dataset,class_field,bbox, X_train, X_test):
+    datasets = ['titanic_c.csv', 'german_credit.csv', 'abalone.csv', 'iris.csv']
+    source_file = f'../datasets/{datasets[number_of_dataset]}'
+    dataset = TabularDataset.from_csv(source_file, class_name=class_field)
+    dataset.df.dropna(inplace=True)
+    dataset.update_descriptor()
+    enc = ColumnTransformerEnc(dataset.descriptor)
+    generator = GeneticGenerator(bbox=bbox, dataset=dataset, encoder=enc, ocr=0.1)
+    surrogate = DecisionTreeSurrogate()
+    tabularLore = Lore(bbox, dataset, enc, generator, surrogate)
 
-    # check
-    # print('Instance ',inst)
-    # print('True class ',Y_test_g.iloc[inst_num])
-    # print('Predicted class ',bb.predict(inst.reshape(1, -1)))
-    # print('Predicted Probability', probabilities[inst_num])
+    instance = X_test[0]
+    #neighbour = generator.generate(instance,200,dataset.descriptor,)
+    l_exp= tabularLore.explain(instance)
+    print(l_exp)
 
-    predicted_class = bb.predict(inst.reshape(1, -1))
-    predicted_proba = bb.predict_proba(inst.reshape(1, -1))
+
+    model = bbox.named_steps['randomforestclassifier']
+    preprocessor = bbox.named_steps['columntransformer']
+
+    X_train_prep = preprocessor.transform(X_train)
+    X_test_prep = preprocessor.transform(X_test)
+
+    s_explainer = shap.TreeExplainer(model, X_train_prep)
+    observation = preprocessor.transform(instance.reshape(1, -1))
+    shap_values = s_explainer.shap_values(observation)
+
+
+    predicted_class = bbox.predict(instance.reshape(1, -1))
+    predicted_proba = bbox.predict_proba(instance.reshape(1, -1))
 
     ## Feature Importance Explanation
-    s_exp = s_explnr.explain(inst)
-    shap_feature_importance = s_exp.exp
-    l_exp = l_explnr.explain(inst)
+
+    #l_exp = l_explnr.explain(inst)
     l_expDict = l_exp.expDict
     # remove key dt from expDict
     l_expDict.pop('dt', None)
@@ -107,13 +129,13 @@ def select_and_explain_instance(inst_num, l_explnr, s_explnr, path, X_test, Y_te
         print('Lore crules', len(crules), 'instance', inst_num)
 
     pe = PlotExplanation(
-        feature_names=feature_names,
+        feature_names=df.columns.tolist(),
         real_feature_names=real_feature_names,
         instance_number=inst_num,
         x_train=X_train,
         expDict=l_expDict,
         feature_importance_type='shap',
-        feature_importance=shap_feature_importance,
+        feature_importance=shap_values,
         numeric_columns=numeric_columns
     )
     features = pe.prepare_rule_descriptor(inst)['features']
@@ -192,7 +214,7 @@ def select_and_explain_instance(inst_num, l_explnr, s_explnr, path, X_test, Y_te
     with open(f'{path}/instance_{inst_num}_lore.txt', "w") as outfile:
         outfile.write(str(l_exp.exp))
 
-    return inst, len(crules)
+    return instance, len(crules)
 
 
 
@@ -207,15 +229,8 @@ if __name__ == '__main__':
     number_of_dataset = 3  # Select the dataset index (0 for Titanic, 1 for German Credit, etc.)
     class_field = "variety"  # Select the proper class field for the dataset
     folder = "iris_explanations" #Select the folder to save the result
-    sample_length = 3  # Number of instances to explain
-    df, class_field = load_data_from_csv(class_field, number_of_dataset)
-    X_test, Y_test, l_explainer, s_explainer, bb, feature_names, numeric_columns, real_feature_names = train_model(df, class_field)
-    integer_list = list(range(X_test.shape[0]))
-    insts_to_compute = random.sample(integer_list, sample_length)
-    for inst_num in insts_to_compute:
-        print(f'Processing Instance {inst_num}',f'instance number {insts_to_compute.index(inst_num) + 1} out of {insts_to_compute}' )
-        _, ncr = select_and_explain_instance(inst_num, l_explainer, s_explainer, f'../d3_sandbox/static/{folder}', X_test, Y_test, bb, feature_names, real_feature_names, X_test, numeric_columns)
-        num_try = 0
-        while (ncr <= 1 and num_try < 5):
-            _, ncr = select_and_explain_instance(inst_num, l_explainer, s_explainer, f'../d3_sandbox/static/{folder}', X_test, Y_test, bb, feature_names, real_feature_names, X_test, numeric_columns)
-            num_try += 1
+    sample_length = 1  # Number of instances to explain
+    df, preprocessor, class_field = load_data_from_csv(class_field, number_of_dataset)
+    model, X_test, X_train =  train_model(df, preprocessor, class_field)
+
+    instance = select_and_explain_instance(number_of_dataset, class_field, model, X_train, X_test)
