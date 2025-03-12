b4e1af3b40ca2d3c87556dbbdf3c9cd796a8307b
Salvo Rinzivillo <rinzivillo@isti.cnr.it>
2025-03-12 16:07:13 +0100

    [Python] removing the ordinal types

--- notebooks/JSONExporter_LORE_sa.py
+++ notebooks/JSONExporter_LORE_sa.py
@@ -102,7 +102,7 @@ def select_and_explain_instance(number_of_dataset,class_field,bbox, X_train, X_t
     surrogate = DecisionTreeSurrogate()
     tabularLore = Lore(bbox, dataset, enc, generator, surrogate)
 
-    inst_num = 285 #random.randint(0, len(X_test))
+    inst_num = random.randint(0, len(X_test))
     instance = X_test[inst_num]
     true_class = y_test[inst_num]
     #neighbour = generator.generate(instance,200,dataset.descriptor,)
@@ -220,89 +220,12 @@ def select_and_explain_instance(number_of_dataset,class_field,bbox, X_train, X_t
 
     ## Feature Importance Explanation
 
-    #l_exp = l_explnr.explain(inst)
-    l_expDict = l_exp
     # remove key dt from expDict
-    crules = l_expDict['counterfactuals']
+    crules = l_exp['counterfactuals']
     if len(crules) > 0:
         print('Lore crules', len(crules), 'instance', inst_num)
 
-    real_numerical_features_names = list(dataset.descriptor['numeric'].keys())
-    real_categorical_features_names = list(dataset.descriptor['categorical'].keys())
-    real_feature_names = real_numerical_features_names + real_categorical_features_names
-    #
-    # pe = PlotExplanation(
-    #     feature_names=df.columns.tolist(),
-    #     real_feature_names=real_feature_names,
-    #     instance_number=inst_num,
-    #     x_train=dataset.df,
-    #     expDict=l_expDict,
-    #     feature_importance_type='lime',
-    #     feature_importance=feat_importance,
-    #     numeric_columns=real_numerical_features_names
-    # )
-    # features = pe.prepare_rule_descriptor(instance)['features']
-    # add categorical info
-    categorical_features_info = {
-        'account_check_status': {
-            'is_ordinal': True,
-            'order': ['no checking account', '< 0 DM', '0 <= ... < 200 DM',
-                      '>= 200 DM / salary assignments for at least 1 year']  # Descendant order
-        },
-        'credit_history': {
-            'is_ordinal': True,
-            'order': ['critical account/ other credits existing (not at this bank)', 'delay in paying off in the past',
-                      'existing credits paid back duly till now', 'all credits at this bank paid back duly',
-                      'no credits taken/ all credits paid back duly']
-        },
-        'purpose': {
-            'is_ordinal': False
-        },
-        'savings': {
-            'is_ordinal': True,
-            'order': ['unknown/ no savings account', '... < 100 DM', '100 <= ... < 500 DM', '500 <= ... < 1000 DM ',
-                      '.. >= 1000 DM ']
-        },
-        'present_emp_since': {
-            'is_ordinal': True,
-            'order': ['unemployed', '... < 1 year ', '1 <= ... < 4 years', '4 <= ... < 7 years', '.. >= 7 years']
-        },
-        'personal_status_sex': {
-            'is_ordinal': False
-        },
-        'other_debtors': {
-            'is_ordinal': False
-        },
-        'property': {
-            'is_ordinal': False
-        },
-        'other_installment_plans': {
-            'is_ordinal': True,
-            'order': ['None', 'bank', 'stores']
-        },
-        'housing': {
-            'is_ordinal': True,
-            'order': ['rent', 'for free', 'own']
-        },
 
-        'job': {
-            'is_ordinal': False
-        },
-
-        'telephone': {
-            'is_ordinal': False
-        },
-        'foreign_worker': {
-            'is_ordinal': False
-        },
-    }
-
-    for feature_entry in features:
-        feature_name = feature_entry.get("rname")
-        if feature_entry.get("type") == "categorical" and feature_name in categorical_features_info:
-            feature_entry["is_ordinal"] = categorical_features_info[feature_name]["is_ordinal"]
-            if feature_entry["is_ordinal"]:
-                feature_entry["order"] = categorical_features_info[feature_name]["order"]
     output_data = {
         "features": features,
         "predicted_class": predicted_class[0],
