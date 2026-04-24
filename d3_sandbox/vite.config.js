2f2c9c2c126de22060f786a2d57a926612563f3f
Salvo Rinzivillo <rinzivillo@isti.cnr.it>
2026-04-24 12:46:00 +0200

    [JS] adding base dir for deployment

--- d3_sandbox/vite.config.js
+++ d3_sandbox/vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path';
 export default defineConfig({
   // index.html is at the project root
   root: '.',
-
+  base: '/TRACE/',
   // Serve the 'public' directory (which contains a 'static' symlink) at '/'
   // so that /static/... URLs resolve correctly
   publicDir: 'public',
